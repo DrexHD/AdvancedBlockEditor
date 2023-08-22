75814cfd2719f96abe8ddaf48efbcd8ad185f68d
Drex <nicknamedrex@gmail.com>
2023-08-22 02:09:48 +0200

    Add command permissions

--- src/main/java/me/drex/advancedblockeditor/command/AdvancedBlockEditorCommand.java
+++ src/main/java/me/drex/advancedblockeditor/command/AdvancedBlockEditorCommand.java
@@ -4,40 +4,38 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.LiteralCommandNode;
-import me.drex.advancedblockeditor.gui.MainGui;
-import me.drex.advancedblockeditor.gui.util.EditingContext;
-import me.drex.advancedblockeditor.util.BlockDisplayFactory;
 import me.drex.advancedblockeditor.util.Tool;
-import me.drex.advancedblockeditor.util.interfaces.EditingPlayer;
 import me.drex.message.api.MessageAPI;
-import net.minecraft.commands.CommandBuildContext;
+import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.arguments.blocks.BlockStateArgument;
-import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Display;
-import net.minecraft.world.level.block.state.BlockState;
 
-import static net.minecraft.commands.Commands.argument;
 import static net.minecraft.commands.Commands.literal;
 
 public class AdvancedBlockEditorCommand {
 
-    // TODO: Permissions
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         LiteralCommandNode<CommandSourceStack> commandNode = dispatcher.register(
-                literal("advancedblockeditor").then(
-                        literal("reload").executes(context -> {
+            literal("advancedblockeditor")
+                .requires(Permissions.require("advancedblockeditor.root", 2))
+                .then(
+                    literal("reload").executes(context -> {
                             MessageAPI.reload();
                             return 1;
                         })
+                        .requires(Permissions.require("advancedblockeditor.reload", 2))
                 ).then(
-                        ScaleCommand.build()
+                    ScaleCommand.build()
                 ).then(
-                    literal("tools").executes(AdvancedBlockEditorCommand::giveTools)
+                    literal("tools")
+                        .requires(Permissions.require("advancedblockeditor.tools", 2))
+                        .executes(AdvancedBlockEditorCommand::giveTools)
                 )
         );
-        dispatcher.register(literal("abe").redirect(commandNode));
+        dispatcher.register(
+            literal("abe")
+                .requires(Permissions.require("advancedblockeditor.root", 2))
+                .redirect(commandNode));
     }
 
     private static int giveTools(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
